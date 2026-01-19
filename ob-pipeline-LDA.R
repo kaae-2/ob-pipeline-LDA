5834a9934b4b72ecf545edc47256a25fb64d28c0
helenewegener1 <helene.wegener@gmaill.com>
2026-01-19 10:30:34 +0100

    update

--- ob-pipeline-LDA.R
+++ ob-pipeline-LDA.R
@@ -25,10 +25,45 @@ source_url("https://raw.githubusercontent.com/tabdelaal/CyTOF-Linear-Classifier/
 ## ============================================================
 
 # Path to zipped data
-dataset_path <- "/home/projects/dp_immunoth/people/helweg/projects/benchmarking/prep_data/out"
+# dataset_path <- "/home/projects/dp_immunoth/people/helweg/projects/benchmarking/prep_data/out"
+
+# GET ARGUMENTS 
+parser <- ArgumentParser(description="FCPS caller")
+
+# parser$add_argument('--data.matrix',
+#                     type="character",
+#                     help='gz-compressed textfile containing the comma-separated data to be clustered.')
+# parser$add_argument('--data.true_labels',
+#                     type="character",
+#                     help='gz-compressed textfile with the true labels.')
+parser$add_argument('--train.data.matrix',
+                    type="character",
+                    help='gz-compressed textfile containing the comma-separated data to be clustered.')
+parser$add_argument('--labels_train',
+                    type="character",
+                    help='gz-compressed textfile with the true labels.')
+parser$add_argument('--test.data.matrix',
+                    type="character",
+                    help='gz-compressed textfile containing the comma-separated data to be clustered.')
+parser$add_argument('--labels_test',
+                    type="character",
+                    help='gz-compressed textfile with the true labels.')
+parser$add_argument('--seed',
+                    type="integer",
+                    help='Random seed',
+                    default = 819797,
+                    dest = 'seed')
+parser$add_argument("--output_dir", "-o", dest="output_dir", type="character",
+                    help="output directory where files will be saved", default=getwd())
+parser$add_argument("--name", "-n", dest="name", type="character", help="name of the dataset")
+# parser$add_argument("--method", "-m", dest="method", type="character", help="method")
+
+args <- parser$parse_args()
+
 
 # LOAD TRAINING X
-train_x_path <- glue("{dataset_path}/train_x.zip")
+# train_x_path <- glue("{dataset_path}/train_x.zip")
+train_x_path <- args[['train.data.matrix']]
 train_x_zip_contents <- unzip(train_x_path, list = TRUE)$Name
 csv_files <- train_x_zip_contents[grepl("\\.csv$", train_x_zip_contents)]
 
@@ -38,6 +73,7 @@ train_x_list <- lapply(csv_files, function(f) {
 
 # LOAD TRAINING Y
 train_y_path <- glue("{dataset_path}/train_y.zip")
+train_y_path <- args[['labels_train']]
 train_y_zip_contents <- unzip(train_y_path, list = TRUE)$Name
 csv_files <- train_y_zip_contents[grepl("\\.csv$", train_y_zip_contents)]
 
@@ -46,7 +82,8 @@ train_y_list <- lapply(csv_files, function(f) {
 })
 
 # LOAD TEST X
-test_x_path <- glue("{dataset_path}/test_x.zip")
+# test_x_path <- glue("{dataset_path}/test_x.zip")
+test_x_path <- args[['test.data.matrix']]
 test_x_zip_contents <- unzip(test_x_path, list = TRUE)$Name
 csv_files <- test_x_zip_contents[grepl("\\.csv$", test_x_zip_contents)]
 
@@ -170,7 +207,8 @@ export_list_as_zipped_csv <- function(lst, zip_path) {
   return(zip_path)
 }
 
-export_list_as_zipped_csv(pred_labels_all, "/home/projects/dp_immunoth/people/helweg/projects/benchmarking/ob-pipeline-LDA/out/labels.zip")
+# export_list_as_zipped_csv(pred_labels_all, "/home/projects/dp_immunoth/people/helweg/projects/benchmarking/ob-pipeline-LDA/out/labels.zip")
+export_list_as_zipped_csv(pred_labels_all, args[['output_dir']])
 
 
 # Delete tmp folder now that we used it
